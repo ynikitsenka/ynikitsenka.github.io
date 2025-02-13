a2105662d643e1cd68d6ccd31f2dfda26c6573cc
ynikitsenka <liza.nickita@gmail.com>
2025-02-13 12:45:17 -0500

    Update script.js

--- portfolio/script.js
+++ portfolio/script.js
@@ -6,3 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error("Error loading navigation:", error));
 });
+
+ fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML("beforeend", data);
+        })
+        .catch(error => console.error("Error loading footer:", error));
+});
