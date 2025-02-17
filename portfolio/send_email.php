744cd1dda04ad5b8efc26b5b1417d591e01b5c56
ynikitsenka <liza.nickita@gmail.com>
2025-02-17 09:14:18 -0500

    Update send_email.php

--- portfolio/send_email.php
+++ portfolio/send_email.php
@@ -1,21 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $message = htmlspecialchars($_POST["message"]);
-
-    $to = "liza.nickita@gmail.com";  
-    $subject = "New Message from Contact Form";
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-    $fullMessage = "Email: $email\n\n";
-    $fullMessage .= "Message:\n$message\n";
-
-    mail($to, $subject, $fullMessage, $headers);
-
-    echo "Message sent! I'll get back to you soon.";
-} else {
-    echo "Error! Please try again.";
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    
+    $to = "liza.nickita@gmail.com";
+    $headers = "From: $email\r\nReply-To: $email\r\n";
+    
+    mail($to, $subject, $message, $headers);
+    
+    echo "Message sent successfully!";
 }
 ?>
